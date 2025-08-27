54cb88641c782c2f29cc2be2191411cba09b1502
= <=>
2025-08-27 15:22:47 +0700

    Until Part 8

---
+++ modules/station/router.go
@@ -0,0 +1,40 @@
+package station
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/oktaharis/mrt-schedules/common/response"
+)
+
+func Initiate(router *gin.RouterGroup) {
+	stationService := NewService()
+
+	station := router.Group("/stations")
+	station.GET("", func(c *gin.Context) {
+		GetAllStation(c, stationService)
+	})
+}
+
+func GetAllStation(c *gin.Context, service Service) {
+	datas, err := service.GetAllStation()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.ApiResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		},
+		)
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		response.ApiResponse{
+			Success: true,
+			Message: "success",
+			Data:    datas,
+		},
+	)
+}
---
+++ modules/station/service.go
@@ -0,0 +1,49 @@
+package station
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/oktaharis/mrt-schedules/common/client"
+)
+
+type Service interface {
+	GetAllStation() (response []StationResponse, err error)
+}
+
+type service struct {
+	client *http.Client
+}
+
+func NewService() Service {
+	return &service{
+		client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (s *service) GetAllStation() (response []StationResponse, err error) {
+	// Layer Service
+
+	url := "https://jakartamrt.co.id/id/val/stasiuns"
+
+	byteResponse, err := client.DoRequest(s.client, url)
+
+	if err != nil {
+		return
+	}
+
+	var stations []Station
+
+	err = json.Unmarshal(byteResponse, &stations)
+	
+	for _, item := range stations {
+		response = append(response, StationResponse{
+			Id: item.Id, 
+			Name: item.Name,
+		})
+	}
+	return
+}
